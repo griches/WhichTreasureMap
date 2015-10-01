d18ec0ad32634b0d748909166657ab31baf35446
Gary Riches <garyriches@me.com>
2015-10-01 09:33:13 +0100

    Initial Commit

---
+++ whichTreasureMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  whichTreasureMap
+//
+//  Created by Gary Riches on 01/10/2015.
+//  Copyright (c) 2015 Gary Riches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ whichTreasureMap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  whichTreasureMap
+//
+//  Created by Gary Riches on 01/10/2015.
+//  Copyright (c) 2015 Gary Riches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
